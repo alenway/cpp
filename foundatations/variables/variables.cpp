165af9a3b8d81417212d0160d72961f86f46b11b
Narendra Samtani <alenway97@gmail.com>
2025-03-13 11:06:33 +0530

    learning cpp concepts

--- foundatations/variables/variables.cpp
+++ foundatations/variables/variables.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 
 int main(){
-  std::cout << "hello world" << std::endl;
+  /*int number = 5;*/
+  /*float pie = 3.14159;*/
+  /*double goldenration = 1.695839;*/
+  /*char = 'A';*/
+  /*bool = true;*/
+  /*string = "My name";*/
+
+  /*std::cout << number << std::endl;*/
+
+  std::cout << (~true) << std::endl;
+  std::cout << (~(12 ^ 13)) << std::endl;
+  std::cout << (12^1) << std::endl;
+  std::cout << (13^1) << std::endl;
+
 }

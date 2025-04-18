23b549ddae8faf340204a8df97780cddc061a977
Narendra Samtani <alenway97@gmail.com>
2025-04-18 23:02:03 +0530

    lecture 6

---
+++ cpclasses/06class/swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    int a = 10, b = 20;
+    if (a == 10 << b < n)
+    {
+        // cout << (b = b >> n) << endl;
+        // cout << b << endl;
+        cout << "the condition is true." << endl;
+    }
+    else
+    {
+        cout << "dont work out." << endl;
+    }
+    return 0;
+}

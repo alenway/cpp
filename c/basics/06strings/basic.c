056cab28ef07086b878afd2e248057db39792eeb
Narendra Samtani <alenway97@gmail.com>
2025-04-27 18:32:43 +0530

    strings in c

---
+++ c/basics/06strings/basic.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    char name[] = "Narendra";
+    return 0;
+}
---
+++ c/basics/06strings/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char * localname = "nitin soni";
+    char name[] = "nitin soni";
+
+    printf("name is : %s\n", localname);
+    return 0;
+}

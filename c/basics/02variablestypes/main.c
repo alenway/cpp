12ba9a47a880a67e02bd1d010f1abf8f9bc145ce
Narendra Samtani <alenway97@gmail.com>
2025-04-27 10:10:47 +0530

    how to initilize variables

---
+++ c/basics/02variablestypes/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a = 12;
+    float f = 0.2398;
+    double d = 3.14159;
+    char c = 'c';
+
+    printf("%d\n",a);
+    printf("%i\n",a);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+    printf("%c\n",c);
+    return 0;
+}

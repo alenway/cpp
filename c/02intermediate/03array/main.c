1f94c29cb6a562886f33a7eb2067da93d3242096
Narendra Samtani <alenway97@gmail.com>
2025-05-10 14:55:16 +0530

    arrays in c

---
+++ c/02intermediate/03array/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // Array declaration in c
+    // datatype arrayname[size];
+    int numbers1[5];              // Declaration
+    int numbers[5] = {1, 2, 3, 4, 5}; // Initialization
+    int a[] = {1, 2, 3}; // size is automatically 3
+    int b[3] = {10, 20, 30};
+    printf("%d\n", b[1]); // Output: 20
+
+
+    return 0;
+}

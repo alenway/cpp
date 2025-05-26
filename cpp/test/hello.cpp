0ce43a676918b7c55f318521661c0cb03e59b777
Narendra Samtani <alenway97@gmail.com>
2025-05-26 17:36:24 +0530

    structure of cpp file and basic

--- cpp/test/hello.cpp
+++ cpp/test/hello.cpp
@@ -1,7 +1,24 @@
+// pre processor directives ( # )
+// custom defined shoud have anomanclature
 #include<iostream>
-
+// header file <iostream>
+// token parsing customization
 using namespace std;
 
+// use can also call only essential methods from namespace 
+using std::cout;
+using std::endl;
+//custom namespace 
+// namespace myspace{
+//   void display(){
+//     // comment
+//   }
+// }
 int main(){
+  // myspace::display();
   cout << "hello world" << endl;
+  // nit picking to only use essential tools in the namespace
+  std::cout << "hello from namespace" << std::endl;
+  //exit code 
+  return 0;
 }

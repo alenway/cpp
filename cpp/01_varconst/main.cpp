8d3ff43c7711a01a6c1d6f8daaab254b61d4de6a
Narendra Samtani <alenway97@gmail.com>
2025-05-27 02:09:23 +0530

    variable is in cpp

---
+++ cpp/01_varconst/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int score;
+  score = 110;
+  int balance = 500;
+  const int id = 8383;
+  // id = 934;
+  cout << id;
+  cout << "welcome to cpp course" << endl;
+  cout << "lsdfj" << endl;
+  return 0;
+}

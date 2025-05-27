ca76da77d54ed3c11facf666e71bc14c1fd67ea8
Narendra Samtani <alenway97@gmail.com>
2025-05-27 18:24:29 +0530

    data modifiers

---
+++ cpp/02_datatypes/modifiers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  unsigned smallTeaPack = 100;
+  long largeTeaStorage = 1000000;
+  short teaShotrs = 2340;
+
+  cout << largeTeaStorage << endl;
+  
+  return 0;
+}
+

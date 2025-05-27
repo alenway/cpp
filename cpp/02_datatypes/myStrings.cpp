1083d9335976791d4e79914406238680569b1029
Narendra Samtani <alenway97@gmail.com>
2025-05-27 18:39:26 +0530

    strings

---
+++ cpp/02_datatypes/myStrings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+  // escape sequence and symbols
+  string myFavouritIcecream = "vanila , chocklate";
+  // escape characters
+  string description = "made with \"best\" coco beans";
+
+  cout << myFavouritIcecream << endl;
+  cout << description << endl;
+
+  return 0;
+}
+

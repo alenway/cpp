6fa853b0db3acd923bf6d926ae00a1072058a64e
Narendra Samtani <alenway97@gmail.com>
2025-06-02 01:03:23 +0530

    datatypes in c

--- c/01beginner/02variablestypes/main.c
+++ c/01beginner/02variablestypes/main.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-int main(){
-    int a = 12;
-    float f = 0.2398;
-    double d = 3.14159;
-    char c = 'c';
-    bool b = true;
+int main() {
+    const int intValue = 12;
+    const float floatValue = 0.2398F;
+    const double doubleValue = 3.14159;
+    const char characterValue = 'c';
+    const bool booleanValue = true;
+    const short shortValue = 10;
+    const long longLongLongValue = 9223372036854775807LL;
+    const long long longLongValue = 9223372036854775807LL;
 
-    printf("%d\n",a);
-    printf("%i\n",a);
-    printf("%f\n",f);
-    printf("%lf\n",d);
-    printf(" %c\n",c);
-    printf("%d", b);
+    const signed int intSignedValue = -12;
+    const unsigned int unsignedIntegerValue = 4294967295U;
+
+    const int intAuto = 100;
+
+    printf("%d\n", intValue);
+    printf("%i\n", intValue);
+    printf("%f\n", floatValue);
+    printf("%lf\n", doubleValue);
+    printf("%c\n", characterValue);
+    printf("%d\n", booleanValue);
+    printf("%d\n", shortValue);
+    printf("%p\n", (void*)&longLongLongValue);
+    printf("%p\n", (void*)&longLongValue);
+    printf("%d\n", intSignedValue);
+    printf("%u\n", unsignedIntegerValue);
+    printf("%d\n", intAuto);
+
     return 0;
 }
